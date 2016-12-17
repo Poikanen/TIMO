ac304daf7c9ad4488f3c15a74f522811681f8303
Toivo <Toivo@Glaurung>
2016-12-17 15:42:00 +0200

    Created DataBuilder, SmartPost & GeoPoint classes, added constructors and getters to SmartPost & GeoPoint

---
+++ TIMO/src/timo/SmartPost.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timo;
+
+/**
+ *
+ * @author Toivo
+ */
+public class SmartPost {
+    
+    public class GeoPoint{
+        private double lat, lon;
+
+        public GeoPoint(double lat, double lon) {
+            this.lat = lat;
+            this.lon = lon;
+        }
+
+        public double getLat() {
+            return lat;
+        }
+
+        public double getLon() {
+            return lon;
+        }
+    }
+    
+    private GeoPoint gp;
+    private String name, city, postnumber;
+
+    public SmartPost(double lat, double lon) {
+        this.gp = new GeoPoint(lat, lon);
+    }
+
+    public GeoPoint getGp() {
+        return gp;
+    }
+}
