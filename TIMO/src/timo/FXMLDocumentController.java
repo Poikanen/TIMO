46242cd04f2894b9d8dba4f10232ba0c1536e0b3
Toivo <Toivo@Glaurung>
2016-12-21 17:45:11 +0200

    Moved adding packages to a new window

--- TIMO/src/timo/FXMLDocumentController.java
+++ TIMO/src/timo/FXMLDocumentController.java
@@ -33,18 +33,6 @@ public class FXMLDocumentController implements Initializable {
     private WebView wvMap;
     @FXML
     private ComboBox<SmartPost> cbSmartPost;
-    @FXML
-    private ComboBox<Item> cbItem;
-    @FXML
-    private ComboBox<Package> cbPackage;
-    @FXML
-    private ComboBox<String> cbStartCity;
-    @FXML
-    private ComboBox<SmartPost> cbStartSmartPost;
-    @FXML
-    private ComboBox<String> cbDestinationCity;
-    @FXML
-    private ComboBox<SmartPost> cbDestinationSmartPost;
     
     private DataBuilder db;
     private Storage storage;
@@ -59,18 +47,6 @@ public class FXMLDocumentController implements Initializable {
         storage = Storage.getInstance();
         
         cbSmartPost.getItems().addAll(db.getAllSmartPosts());
-        cbStartCity.getItems().addAll(db.getCities());
-        cbDestinationCity.getItems().addAll(db.getCities());
-        
-        cbPackage.getItems().add(new PackageFirstCategory(new Item(), new SmartPost(), new SmartPost()));
-        cbPackage.getItems().add(new PackageSecondCategory(new Item(), new SmartPost(), new SmartPost()));
-        cbPackage.getItems().add(new PackageThirdCategory(new Item(), new SmartPost(), new SmartPost()));
-        
-        cbItem.getItems().add(new Item());
-        cbItem.getItems().add(new Sofa());
-        cbItem.getItems().add(new Laptop());
-        cbItem.getItems().add(new Teacup());
-        cbItem.getItems().add(new Plushie());
     }
 
     @FXML
@@ -93,17 +69,6 @@ public class FXMLDocumentController implements Initializable {
         //System.out.println(script);
         wvMap.getEngine().executeScript(script);
         textInfoBox.setText("SmartPost lisätty.\n");
-    }
-
-    @FXML
-    private void handleCreatePacket(ActionEvent event) {
-        Package tmpPkg = cbPackage.getValue().getCopy();
-        tmpPkg.setItem(new Item(cbItem.getValue()));
-        tmpPkg.setStart(new SmartPost(cbStartSmartPost.getValue()));
-        tmpPkg.setDestination(new SmartPost(cbDestinationSmartPost.getValue()));
-        
-        storage.addPackage(tmpPkg);
-        textInfoBox.setText("Paketti luotu.\n");
     }
 
     @FXML
@@ -130,35 +95,25 @@ public class FXMLDocumentController implements Initializable {
     }
 
     @FXML
-    private void handleStartCityChange(ActionEvent event) {
-        cbStartSmartPost.getItems().clear();
-        cbStartSmartPost.getItems().addAll(db.getCitysSmartPosts(cbStartCity.getValue()));
-    }
-
-    @FXML
-    private void handleDestinationCityChange(ActionEvent event) {
-        cbDestinationSmartPost.getItems().clear();
-        cbDestinationSmartPost.getItems().addAll(db.getCitysSmartPosts(cbDestinationCity.getValue()));
-    }
-
-    @FXML
-    private void displayClassInfo(ActionEvent event) {
-        if(cbPackage.getValue().getCategory().equals("1")){
-            textInfoBox.setText("1. luokan paketit kulkevat nopeasti alle 150km päähän.\n");
-        }else if(cbPackage.getValue().getCategory().equals("2")){
-            textInfoBox.setText("2. luokan paketit kulkevat luotettavasti kaikkialle Suomeen.\n");
-        }else if(cbPackage.getValue().getCategory().equals("3")){
-            textInfoBox.setText("3. luokan paketteja käytetään TIMOjen stressinpurkuun joten sisäsltö saattaa hajota matkalla.\n");
-        }
-    }
-
-    @FXML
     private void displayLog(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("LogWindow.fxml"));
             Stage stage = new Stage();
             stage.setScene(new Scene((Pane)loader.load()));
             loader.<LogWindowController>getController().initData(storage.getLog());
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void handleOpenAddPackageWindow(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("AddPackageWindow.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene((Pane)loader.load()));
+            loader.<AddPackageWindowController>getController().initData(db);
             stage.show();
         } catch (IOException ex) {
             Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
