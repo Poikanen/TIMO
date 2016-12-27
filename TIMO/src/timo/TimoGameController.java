77e8db7c9dd7e237e13e1b748191297e4c9dd958
tommi <tommi@tommis-mbp>
2016-12-27 03:01:00 +0200

    Timo Game GUI with scaling.

--- TIMO/src/timo/TimoGameController.java
+++ TIMO/src/timo/TimoGameController.java
@@ -6,8 +6,15 @@
 package timo;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.web.WebView;
 
 /**
  * FXML Controller class
@@ -16,12 +23,35 @@ import javafx.fxml.Initializable;
  */
 public class TimoGameController implements Initializable {
 
+    @FXML
+    private WebView wvMap;
+    @FXML
+    private TextArea packageInfoField;
+    @FXML
+    private ListView<?> packageListView;
+
+    
+    private DataBuilder db;
+    private Storage storage;
+    private ArrayList<SmartPost> smartPosts;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        wvMap.getEngine().load(getClass().getResource("index.html").toExternalForm());
+        db = new DataBuilder();
+        storage = Storage.getInstance();
+        smartPosts = new ArrayList();
+        smartPosts.addAll(db.getAllSmartPosts());
+        packageListView.getItems().addAll((Collection)smartPosts);
+        
     }    
+
+    @FXML
+    private void sendTimo(ActionEvent event) {
+        
+    }
     
 }
